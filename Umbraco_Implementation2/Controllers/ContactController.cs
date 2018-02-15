d64cf9bade28b02338b02e34726cb3b28ebee605
AlexFursenkoOd <andersen.od.ua@gmail.com>
2018-02-15 16:07:15 +0200

    Add project files.

---
+++ Umbraco_Implementation2/Controllers/ContactController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco_Implementation2.Controllers
+{
+    [Route("api/{controller}/{action}")]
+    public class ContactController : SurfaceController
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contentItems = this.Services.ContentService.GetByLevel(1);
+            foreach(var content in contentItems)
+            {
+                Console.WriteLine("!");
+            }
+            return View();
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contact/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Umbraco_Implementation2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+
+namespace Umbraco_Implementation2.Controllers
+{
+    public class HomeController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            //Do some stuff here, then return the base method
+            var a = 2;
+            return base.Index(model);
+        }
+
+    }
+}
